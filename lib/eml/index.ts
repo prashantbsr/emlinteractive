a142ba29b45e8d4de6ea70a4da1d2cf31a90ea52
prashantbsr <prashant.bsr.33@gmail.com>
2026-04-19 15:15:52 +0530

    feat(eml): add barrel index.ts re-exporting the public eml API

---
+++ lib/eml/index.ts
@@ -0,0 +1,7 @@
+export * from "./types";
+export * from "./evaluate";
+export * from "./parser";
+export * from "./serialize";
+export * from "./depth";
+export * from "./trace";
+export * from "./library";
